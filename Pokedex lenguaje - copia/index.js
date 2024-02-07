1074544d0c4ef20c1d60b1ac0ba2da0bdcd2c49d
Noe1410 <145497072+Noe1410@users.noreply.github.com>
2024-02-07 17:20:40 +0100

    cambios

--- Pokedex lenguaje - copia/index.js
+++ Pokedex lenguaje - copia/index.js
@@ -17,7 +17,7 @@ async function initializePokemon() {
     }
     pokeDraw(pokemons);
 }
-    
+
 initializePokemon();
 
 function pokeDraw(pokemons){
@@ -28,8 +28,10 @@ function pokeDraw(pokemons){
         div.innerHTML = `<img id="imagen${i + 1}" height="140px" src=${pokemons[i].image} />
                          <h1 class="id">#${pokemons[i].index.toString().padStart(3, '0')}</h1>
                          <h1 class="nombre" id="name${i + 1}">${pokemons[i].name}</h1>
-                         <span id="tipo">${pokemons[i].types[0]}</span>
-                         <span id="tipo">
+                         ${getColors(pokemons[i].types[0])}
+                         <span class="tipo">${pokemons[i].types[0]}</span>
+                         ${getColors(pokemons[i].types[1])}
+                         <span class="tipo">
                              ${pokemons[i].types[1] ? pokemons[i].types[1] : ''}
                          </span>`;
         tarjeta.appendChild(div);
@@ -44,7 +46,6 @@ input.addEventListener("keyup", filtro);
     let filtrado = [];
     pokeContainer.innerHTML = '';
     if(inputValue.length == 0){ 
-        console.log(inputValue);
         pokeDraw(pokemons);
     }else {
         for(let i = 0; i < pokemons.length; i++){
@@ -55,6 +56,83 @@ input.addEventListener("keyup", filtro);
         pokeDraw(filtrado);
     } 
  }
+ const tipoElements = document.getElementsByClassName('tipo');
+ function getColors (t){
+    console.log(t);
+    for (let i = 0; i < tipoElements.length; i++) {
+        const tipoElement = tipoElements[i];
+    switch (t) {
+        case "Planta":
+            tipoElement.classList.add('planta');
+            break;
+
+        case "Fuego":
+            tipoElement.classList.add('fuego');
+            break;
+
+        case "Agua":
+            tipoElement.classList.add('agua');
+            break;
+
+        case "Electrico":
+            tipoElement.classList.add('electrico');
+            break;
+
+        case "Veneno":
+            tipoElement.classList.add('veneno');
+            break;
+
+        case "Volador":
+            tipoElement.classList.add('volador');
+            break;
+
+        case "Bicho":
+            tipoElement.classList.add('bicho');
+            break;
+
+        case "Normal":
+            tipoElement.classList.add('normal');
+            break;
+
+        case "Tierra":
+            tipoElement.classList.add('tierra');
+            break;
+
+        case "Hada":
+            tipoElement.classList.add('hada');
+            break;
+
+        case "Lucha":
+            tipoElement.classList.add('lucha');
+            break;
+
+        case "Psiquico":
+            tipoElement.classList.add('psiquico');
+            break;
+
+        case "Roca":
+            tipoElement.classList.add('roca');
+            break;
+
+        case "Acero":
+            tipoElement.classList.add('acero');
+            break;
+
+        case "Hielo":
+            tipoElement.classList.add('hielo');
+            break;
+
+        case "Fantasma":
+            tipoElement.classList.add('fantasma');
+            break;
+
+        case "Dragon":
+            tipoElement.classList.add('dragon');
+            break;
+        
+    }
+ }
+}
 
 async function getPokemon(id){
     const pokemonJson = await getData(`https://pokeapi.co/api/v2/pokemon/${id}`);
